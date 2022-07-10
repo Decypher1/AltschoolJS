3879b4c8463271873e336b52f15e5b07c580f7f6
Martins Umekwe <mumekwe2@gmail.com>
2022-07-10 13:45:12 +0000

    check challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  
+  for (n = 0; n < arrayOfNumbers.length; n++) {
+    sum += arrayOfNumbers[n];
+  }
+    return (sum);
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let countEven = 0;
+
+  for(n = 0; n < arrayOfNumbers.length; n++) {
+    if (arrayOfNumbers[n] % 2 == 0) {
+      countEven++;
+    }
+  }
+    return (countEven);
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
